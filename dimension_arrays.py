d380aa82af1afb5349c3ecfe85f08a8fd1c35a0e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-11-24 18:24:00 +0500

    Check the number of dimensions

--- dimension_arrays.py
+++ dimension_arrays.py
@@ -15,3 +15,10 @@ print(arr_2d)
 arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
 print(arr_3d)
+
+# Check the number of dimensions
+print("Number of dimensions:")
+print("0D array:", arr_0d.ndim)
+print("1D array:", arr_1d.ndim)
+print("2D array:", arr_2d.ndim)
+print("3D array:", arr_3d.ndim)
