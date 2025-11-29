ee32f79c5ad64ba984d6935fbb8df0e63a8fe19a
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-11-29 07:09:56 +0500

    Accessing index of 2D Array

--- 03_array_indexing.py
+++ 03_array_indexing.py
@@ -9,3 +9,7 @@ print(arr[1]) # 2
 # Array indexing is the same as accessing an array element.
 
 print(arr[2] + arr[3]) # 3 + 4 = 7
+
+arr_2d = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', arr_2d[0, 1])
