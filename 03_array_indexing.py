24f2984e3404cf18ce84ab9d84392f7f6baa8a03
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-11-29 07:08:51 +0500

    Results of operations

--- 03_array_indexing.py
+++ 03_array_indexing.py
@@ -2,10 +2,10 @@ import numpy as np
 
 arr = np.array([1, 2, 3, 4])
 
-print(arr[0])
+print(arr[0]) # 1
 
-print(arr[1])
+print(arr[1]) # 2
 
 # Array indexing is the same as accessing an array element.
 
-print(arr[2] + arr[3])
+print(arr[2] + arr[3]) # 3 + 4 = 7
