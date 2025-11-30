debf379204d331f38f05ea52078c711b44710c96
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-11-30 14:34:41 +0500

    Explaination of 3D array indexing

--- 03_array_indexing.py
+++ 03_array_indexing.py
@@ -16,6 +16,33 @@ arr_2d = np.array([[1,2,3,4,5],
 print('2nd element on 1st row: ', arr_2d[0, 1]) # R C -> 2
 print('5th element on 2nd row: ', arr_2d[1, 4]) # R C -> 10
 
-arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+arr_3d = np.array([[[1, 2, 3], 
+                    [4, 5, 6]], 
+                   [[7, 8, 9], 
+                    [10, 11, 12]]
+                    ])
 
 print(arr_3d[0, 1, 2]) # 6
+
+# And this is why:
+
+# The first number represents the first dimension, which contains two arrays:
+# [[1, 2, 3], [4, 5, 6]]
+# and:
+# [[7, 8, 9], [10, 11, 12]]
+# Since we selected 0, we are left with the first array:
+# [[1, 2, 3], [4, 5, 6]]
+
+# The second number represents the second dimension, which also contains two arrays:
+# [1, 2, 3]
+# and:
+# [4, 5, 6]
+# Since we selected 1, we are left with the second array:
+# [4, 5, 6]
+
+# The third number represents the third dimension, which contains three values:
+# 4
+# 5
+# 6
+# Since we selected 2, we end up with the third value:
+# 6
