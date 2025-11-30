3ccd7d9503bc6c645f6893d0fe21d9dd60e1faa9
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-11-30 14:31:08 +0500

    Indexing of 3D array

--- 03_array_indexing.py
+++ 03_array_indexing.py
@@ -15,3 +15,7 @@ arr_2d = np.array([[1,2,3,4,5],
 
 print('2nd element on 1st row: ', arr_2d[0, 1]) # R C -> 2
 print('5th element on 2nd row: ', arr_2d[1, 4]) # R C -> 10
+
+arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(arr_3d[0, 1, 2]) # 6
