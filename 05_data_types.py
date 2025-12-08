5412ce3c97c9508263ed81b75a9ca0e3010238dd
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-12-08 22:40:32 +0500

    A non integer string like 'a' can not be converted to integer (will raise an error)

--- 05_data_types.py
+++ 05_data_types.py
@@ -19,3 +19,8 @@ arr3 = np.array([1, 2, 3, 4], dtype='i4')
 
 print(arr3)
 print(arr3.dtype)
+
+
+# A non integer string like 'a' can not be converted to integer (will raise an error):
+
+arr4 = np.array(['a', '2', '3'], dtype='i') # Error
