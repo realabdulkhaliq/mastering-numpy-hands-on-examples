9e3f98d0343b15588b3be738d4681da23d2b6ed7
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-12-08 22:37:14 +0500

    Creating Arrays With a Defined Data Type

--- 05_data_types.py
+++ 05_data_types.py
@@ -3,3 +3,10 @@ import numpy as np
 arr = np.array([1, 2, 3, 4])
 
 print(arr.dtype)
+
+# Creating Arrays With a Defined Data Type
+
+arr2 = np.array([1, 2, 3, 4], dtype='S')
+
+print(arr2)
+print(arr2.dtype)
