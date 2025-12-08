a1c499d392c2ede7045d4640ea2f0ae936c3f776
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-12-08 22:38:23 +0500

    Create an array with data type 4 bytes integer

--- 05_data_types.py
+++ 05_data_types.py
@@ -10,3 +10,12 @@ arr2 = np.array([1, 2, 3, 4], dtype='S')
 
 print(arr2)
 print(arr2.dtype)
+
+# We can define size as well
+
+# Create an array with data type 4 bytes integer:
+
+arr3 = np.array([1, 2, 3, 4], dtype='i4')
+
+print(arr3)
+print(arr3.dtype)
